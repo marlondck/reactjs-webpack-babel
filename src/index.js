75f0957c5e397fa128b14b0bcdd8330188213a06
marlondck <15050029+marlondck@users.noreply.github.com>
2021-10-22 14:14:32 -0400

    primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import { soma } from './soma';
+import React from "react";
+import { render } from 'react-dom';
 
-console.log(soma(1, 2))
+import App from './App'
+
+render(<App />, document.getElementById('app'));
