61a48901c18bf655e25f07c2825682dade935713
marlondck <15050029+marlondck@users.noreply.github.com>
2021-10-22 14:45:22 -0400

    Estado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from './components/Header';
-import Menu from './components/Menu';
 
 function App() {
+  const [projects, setProjects] = useState(['Dev de App', 'Front-end web'])
+
+  function handleAddProjet() {
+    setProjects([...projects, 'novo valor'])
+    console.log(projects)
+  }
+
   return (
     <>
      <Header title="Home page"/>
-      <Menu>
-        <ul>
-          <li>Home</li>
-          <li>Sobre</li>
-        </ul>
-      </Menu>
+      <ul>
+        { projects.map(project => {
+          return <li key={project}>{project}</li>
+        }) }
+      </ul>
+
+      <br />
+
+      <button type="button" onClick={handleAddProjet}>Adicionar</button>
     </>
   )
   
