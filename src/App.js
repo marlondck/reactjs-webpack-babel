f3edc3d4db523b801d3dc7a310e48bf9018c12f9
marlondck <15050029+marlondck@users.noreply.github.com>
2021-10-22 14:29:43 -0400

    Propriedades

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
-  return <Header/>
+  return (
+    <>
+     <Header title="Home page"/>
+      <Menu>
+        <ul>
+          <li>Home</li>
+          <li>Sobre</li>
+        </ul>
+      </Menu>
+    </>
+  )
+  
 }
 
 export default  App;
